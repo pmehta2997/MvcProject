4015971ef62470bfbf6b0540008dd129da92d896
pmehta2997 <pragyam20@gmail.com>
2025-03-22 23:45:14 -0700

    Add project files.

---
+++ AssignmentProjectMVC/AssignmentProjectMVC/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AssignmentProjectMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AssignmentProjectMVC.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Post> Post { get; set; }
+    }
+}
---
+++ AssignmentProjectMVC/AssignmentProjectMVC/Models/Post.cs
@@ -0,0 +1,10 @@
+﻿namespace AssignmentProjectMVC.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+
+    }
+}
